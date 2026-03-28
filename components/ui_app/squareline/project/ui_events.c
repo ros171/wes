b5d785eb5a1dacd8ff8c93bb13ced52b64a74e18
Lovro Rosandic <lovro.rosandic@gmail.com>
2026-03-28 17:34:53 +0100

    Dodani screen 1-3

--- components/ui_app/squareline/project/ui_events.c
+++ components/ui_app/squareline/project/ui_events.c
@@ -7,40 +7,13 @@
 
 void dark_mode_on(lv_event_t *e)
 {
-    lv_color_t bg_color_white = lv_color_hex(0x111111);
-    lv_color_t bg_color_black = lv_color_hex(0xFFFFFF);
-
-    lv_obj_add_state(ui_Dark_Mode_Switch, LV_STATE_CHECKED);
-    lv_obj_add_state(ui_Dark_Mode_Switch_Color, LV_STATE_CHECKED);
-    lv_obj_add_state(ui_Dark_Mode_Switch_Sett, LV_STATE_CHECKED);
-
-    lv_obj_set_style_text_color(ui_Dark_Mode_Label, bg_color_black, 0);
-    lv_obj_set_style_text_color(ui_Dark_Mode_Label_Color, bg_color_black, 0);
-    lv_obj_set_style_text_color(ui_Dark_Mode_Label_Sett, bg_color_black, 0);
-    lv_obj_set_style_bg_color(ui_Home_Scr, bg_color_white, 0);
-    lv_obj_set_style_bg_color(ui_Color_Scr, bg_color_white, 0);
-    lv_obj_set_style_bg_color(ui_Settings_Scr, bg_color_white, 0);
-
-    lv_obj_set_style_bg_color(ui_Settings_scroll_panel, bg_color_white, 0);
-    lv_obj_set_style_border_color(ui_Settings_scroll_panel, bg_color_white, 0);
 }
 
 void dark_mode_off(lv_event_t *e)
 {
-    lv_color_t bg_color_white = lv_color_hex(0x111111);
-    lv_color_t bg_color_black = lv_color_hex(0xFFFFFF);
-
-    lv_obj_clear_state(ui_Dark_Mode_Switch, LV_STATE_CHECKED);
-    lv_obj_clear_state(ui_Dark_Mode_Switch_Color, LV_STATE_CHECKED);
-    lv_obj_clear_state(ui_Dark_Mode_Switch_Sett, LV_STATE_CHECKED);
-
-    lv_obj_set_style_text_color(ui_Dark_Mode_Label, bg_color_white, 0);
-    lv_obj_set_style_text_color(ui_Dark_Mode_Label_Color, bg_color_white, 0);
-    lv_obj_set_style_text_color(ui_Dark_Mode_Label_Sett, bg_color_white, 0);
-    lv_obj_set_style_bg_color(ui_Home_Scr, bg_color_black, 0);
-    lv_obj_set_style_bg_color(ui_Color_Scr, bg_color_black, 0);
-    lv_obj_set_style_bg_color(ui_Settings_Scr, bg_color_black, 0);
-
-    lv_obj_set_style_bg_color(ui_Settings_scroll_panel, bg_color_black, 0);
-    lv_obj_set_style_border_color(ui_Settings_scroll_panel, bg_color_black, 0);
 }
+
+// Removed references to non-existent screens
+// Available screens: ui_Home_Scr, ui_Color_Scr, ui_Settings_Scr
+// The next_animal() and prev_animal() functions have been removed as they
+// referenced Screen1-Screen8 which do not exist in this project.
